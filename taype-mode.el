3d49f9904af55ef28a809ae1065a5f140e587cbf
Qianchuan Ye <yeqianchuan@gmail.com>
2023-04-16 04:39:29 -0400

    Fix with matching

--- taype-mode.el
+++ taype-mode.el
@@ -92,7 +92,7 @@
                            'symbols))
            ;; The operator regular expressions are not used at the moment.
            (operator-rx (regexp-opt
-                         `("\\" "->" "=>" ":" "=" "|" ","
+                         `("\\" "->" "=>" ":" "=" "|"
                            "<=" "==" "+" "-" "*" "/" "&&" "||"
                            ,(taype-obliv-name "<=") ,(taype-obliv-name "==")
                            ,(taype-obliv-name "+") ,(taype-obliv-name "-")
@@ -117,7 +117,7 @@
            (lam-rx (rx "\\"
                        (group (*? anything))
                        "=>"))
-           (alt-rx (rx (| "with" "|")
+           (alt-rx (rx (| (: symbol-start "with" symbol-end) "|")
                        (* anyspace)
                        (opt var)
                        (group (*? (not "|")))
@@ -132,8 +132,7 @@
                        ?%
                        (* (in alnum "'_"))
                        symbol-end)))
-      `(
-        (,keyword-rx . font-lock-keyword-face)
+      `((,keyword-rx . font-lock-keyword-face)
         (,inst-rx (0 taype-font-lock-obliv-instance-face))
         (,def-fun-rx
           (1 taype-font-lock-governing-face)
@@ -141,8 +140,9 @@
         (,def-type-rx
           (1 taype-font-lock-governing-face)
           (2 font-lock-type-face))
-        (,lam-rx (1 font-lock-variable-name-face))
-        (,alt-rx (1 font-lock-variable-name-face))
+        ;; (,lam-rx (1 font-lock-variable-name-face))
+        ;; (,alt-rx (1 font-lock-variable-name-face))
+        (,operator-rx . font-lock-variable-name-face)
         (,ctor-rx . taype-font-lock-constructor-face)
         (,obliv-ctor-rx . taype-font-lock-constructor-face)
         (,builtin-rx . font-lock-builtin-face)
